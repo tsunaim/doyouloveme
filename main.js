ae0ea3e92492c0059fe92f9085fc01d4ae14358f
Apom <51718198+tsunaim@users.noreply.github.com>
2024-02-14 18:54:31 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const wrapper = document.getElementById("wrapper");
+const gif = document.getElementById("gif");
+const question = document.getElementById("question");
+const yesBtn = document.getElementById("yes-btn");
+const noBtn = document.getElementById("no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "Lo sabía 7w7r ❤✨";
+  gif.src = "https://i.pinimg.com/originals/ce/63/c2/ce63c26f8747e2181594d2db8787c026.gif";
+  noBtn.hidden = true;
+  yesBtn.hidden = true;
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
+
+noBtn.addEventListener("click", () => {
+  question.innerHTML = "";
+  gif.src = "https://i.pinimg.com/originals/03/a4/28/03a428ee705240475480c7722c6c2b3b.gif";
+  noBtn.hidden = true;
+  // yesBtn.hidden = true;
+});
